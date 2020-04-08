cbfc7c2843de24b3c9ac8a33708fe651d371b5f0
Aziz Boudani <boudani@gmail.com>
2020-04-08 15:05:56 +0200

    complet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, einloggen, ausloggen } from './actions';
 function App() {
-  return <div className="App">test</div>;
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+  return (
+    <div className="App">
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+ 5</button>
+      <button onClick={() => dispatch(decrement(3))}>- 3</button>
+      <p></p>
+      <button onClick={() => dispatch(einloggen())}>Einloggen</button>
+      <p></p>
+      <button onClick={() => dispatch(ausloggen())}>Ausloggen</button>
+
+      {isLogged ? (
+        <h3>darf ich sehen, weil ich eingeloggt bin</h3>
+      ) : (
+        <h3>darf ich nicht sehen</h3>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,22 @@
-export const increment = () => {
+export const increment = (x) => {
   return {
     type: 'INCREMENT',
+    payload: x,
   };
 };
-export const decrement = () => {
+export const decrement = (x) => {
   return {
     type: 'DECREMENT',
+    payload: x,
+  };
+};
+export const einloggen = () => {
+  return {
+    type: 'SIGN_IN',
+  };
+};
+export const ausloggen = () => {
+  return {
+    type: 'SIGN_UP',
   };
 };
